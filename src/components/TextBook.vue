3703b9788d68ce800097c8193adb4ddfdc405dcb
fasbender <59934559+fasbender@users.noreply.github.com>
2020-06-24 18:10:20 +0600

    HomePage-3-TextBook

---
+++ src/components/TextBook.vue
@@ -0,0 +1,157 @@
+<template>
+  <!-- HomePage-3 -->
+  <div id="test">
+    <h1 class="display-5 mt-3">Experience the Best @ Testbook.com</h1>
+    <div class="row">
+      <div class="col-md-4 col-lg-3">
+        <div class="card">
+          <div class="card-header bg-white">
+            <img src="..\assets\TextBook\img\Group-4.png" alt />
+            <h4>Experienced Teachers</h4>
+          </div>
+          <div class="card-body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-lg-3">
+        <div class="card">
+          <div class="card-header bg-white">
+            <img src="..\assets\TextBook\img\Group-2.png" alt />
+            <h4>Top Quality Questions</h4>
+          </div>
+          <div class="card-body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-lg-3">
+        <div class="card">
+          <div class="card-header bg-white">
+            <img src="..\assets\TextBook\img\Compound_Path.png" alt />
+            <h4>Personalized, detailed Analysis</h4>
+          </div>
+          <div class="card-body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row" style="margin-top: -18px;">
+      <div class="col-md-4 col-lg-3">
+        <div class="card">
+          <div class="card-header bg-white">
+            <img src="..\assets\TextBook\img\Group-3.png" alt />
+            <h4>Live Tests for Real Exam Experience</h4>
+          </div>
+          <div class="card-body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-lg-3">
+        <div class="card">
+          <div class="card-header bg-white">
+            <img src="..\assets\TextBook\img\Group-1.png" alt />
+            <h4>Community for learning</h4>
+          </div>
+          <div class="card-body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-lg-3">
+        <div class="card">
+          <div class="card-header bg-white">
+            <img src="..\assets\TextBook\img\Group.png" alt />
+            <h4>Bilingual: Hindi and English</h4>
+          </div>
+          <div class="card-body">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  Name: 'Test'
+}
+</script>
+
+<style lang="scss" scoped>
+#test {
+  .card-header {
+    display: flex;
+    img {
+      width: 20px;
+    }
+    h4 {
+      font-size: 20px;
+      margin-left: 10px;
+    }
+  }
+  .card-body {
+    margin-top: -20px;
+    font-size: 0.8rem;
+  }
+  .card {
+    width: 400px;
+    height: 250px;
+  }
+  .card,
+  .card-header {
+    border: none;
+  }
+}
+</style>
