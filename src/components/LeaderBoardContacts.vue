d47330d3979e1ed8a7e6c7e69de17fffdfc6ab95
ashiqurrah7 <ashiqur.rahm@gmail.com>
2020-06-25 21:35:20 +0600

    Leaderboard contacts added

---
+++ src/components/LeaderBoardContacts.vue
@@ -0,0 +1,553 @@
+<template>
+  <div class="d-flex main-container">
+    <div class="container-fluid left-container">
+      <div class="cards d-flex flex-lg-row flex-column justify-content-between">
+        <div class="card" style="width: 18rem;">
+          <div class="text-center">
+            <img
+              src="../assets/Leaderboard/img/girl.png"
+              class="card-img-top mt-1"
+            />
+          </div>
+          <h4 class="text-center">Asma Akter</h4>
+          <h5 class="text-center mb-5">Assistant</h5>
+          <div class="d-flex justify-content-around card-upper">
+            <img src="../assets/Leaderboard/img/graph-dot.png" class="graph" />
+            <p class="text-center card-upper-text">
+              <span class="upper-number">122</span> <br />
+              cccccc
+            </p>
+          </div>
+          <div class="d-flex justify-content-around card-lower">
+            <p class="text-center mt-3 mr-3">AAAAAA</p>
+            <div class="between"></div>
+            <p class="text-center mt-3 ml-3">BBBBBB</p>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <div class="text-center">
+            <img
+              src="../assets/Leaderboard/img/boyShirt.png"
+              class="card-img-top mt-1"
+            />
+          </div>
+          <h4 class="text-center">Abul Kalam</h4>
+          <h5 class="text-center mb-5">Assistant</h5>
+          <div class="d-flex justify-content-around card-upper">
+            <img
+              src="../assets/Leaderboard/img/graph-arrow-down.png"
+              class="graph"
+            />
+            <p class="text-center card-upper-text">
+              <span class="upper-number">108+</span> <br />
+              cccccc
+            </p>
+          </div>
+          <div class="d-flex justify-content-around card-lower">
+            <p class="text-center mt-3 mr-3">AAAAAA</p>
+            <div class="between"></div>
+            <p class="text-center mt-3 ml-3">BBBBBB</p>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <div class="text-center">
+            <img
+              src="../assets/Leaderboard/img/boyTshirt.png"
+              class="card-img-top mt-1"
+            />
+          </div>
+          <h4 class="text-center">Mostafiz Rahman</h4>
+          <h5 class="text-center mb-5">Assistant</h5>
+          <div class="d-flex justify-content-around card-upper">
+            <img
+              src="../assets/Leaderboard/img/graph-arrow-up.png"
+              class="graph"
+            />
+            <p class="text-center card-upper-text">
+              <span class="upper-number">99</span> <br />
+              cccccc
+            </p>
+          </div>
+          <div class="d-flex justify-content-around card-lower">
+            <p class="text-center mt-3 mr-3">AAAAAA</p>
+            <div class="between"></div>
+            <p class="text-center mt-3 ml-3">BBBBBB</p>
+          </div>
+        </div>
+      </div>
+      <br />
+      <h4 class="font-weight-bold">Other Employees</h4>
+      <br />
+      <div class="cards-long d-flex flex-column">
+        <div
+          class="card card-long d-flex flex-row flex-wrap justify-content-between"
+        >
+          <p class="empty"></p>
+          <img
+            src="../assets/Leaderboard/img/selected.png"
+            alt=""
+            class="selected d-none"
+          />
+          <img src="../assets/Leaderboard/img/girl.png" class="card-img mt-1" />
+          <div>
+            <h6>Asma Akter</h6>
+            <p>Assistant</p>
+          </div>
+          <div>
+            <h6>+88 01819123456</h6>
+            <p>AAAAAAAAA</p>
+          </div>
+          <img src="../assets/Leaderboard/img/graph-dot.png" />
+          <div>
+            <h6 class="text-center">122</h6>
+            <p>cccccccc</p>
+          </div>
+          <div>
+            <img
+              src="../assets/Leaderboard/img/hold.png"
+              class="card-img-last"
+            />
+          </div>
+        </div>
+        <div
+          class="card card-long d-flex flex-row flex-wrap justify-content-between"
+        >
+          <p class="empty"></p>
+          <img
+            src="../assets/Leaderboard/img/selected.png"
+            alt=""
+            class="selected d-none"
+          />
+          <img src="../assets/Leaderboard/img/girl.png" class="card-img mt-1" />
+          <div>
+            <h6>Asma Akter</h6>
+            <p>Assistant</p>
+          </div>
+          <div>
+            <h6>+88 01819123456</h6>
+            <p>AAAAAAAAA</p>
+          </div>
+          <img src="../assets/Leaderboard/img/graph-arrow-up.png" />
+          <div>
+            <h6 class="text-center">122</h6>
+            <p>cccccccc</p>
+          </div>
+          <div>
+            <img
+              src="../assets/Leaderboard/img/hold.png"
+              class="card-img-last"
+            />
+          </div>
+        </div>
+        <div
+          class="card card-long d-flex flex-row flex-wrap justify-content-between active"
+        >
+          <p class="empty"></p>
+          <img
+            src="../assets/Leaderboard/img/selected.png"
+            alt=""
+            class="selected"
+          />
+          <img src="../assets/Leaderboard/img/girl.png" class="card-img mt-1" />
+          <div>
+            <h6>Asma Akter</h6>
+            <p>Assistant</p>
+          </div>
+          <div>
+            <h6>+88 01819123456</h6>
+            <p>AAAAAAAAA</p>
+          </div>
+          <img src="../assets/Leaderboard/img/graph-arrow-down.png" />
+          <div>
+            <h6 class="text-center">122</h6>
+            <p>cccccccc</p>
+          </div>
+          <div>
+            <img
+              src="../assets/Leaderboard/img/hold.png"
+              class="card-img-last"
+            />
+          </div>
+        </div>
+        <div
+          class="card card-long d-flex flex-row flex-wrap justify-content-between"
+        >
+          <p class="empty"></p>
+          <img
+            src="../assets/Leaderboard/img/selected.png"
+            alt=""
+            class="selected d-none"
+          />
+          <img src="../assets/Leaderboard/img/girl.png" class="card-img mt-1" />
+          <div>
+            <h6>Asma Akter</h6>
+            <p>Assistant</p>
+          </div>
+          <div>
+            <h6>+88 01819123456</h6>
+            <p>AAAAAAAAA</p>
+          </div>
+          <img src="../assets/Leaderboard/img/graph-dot.png" />
+          <div>
+            <h6 class="text-center">122</h6>
+            <p>cccccccc</p>
+          </div>
+          <div>
+            <img
+              src="../assets/Leaderboard/img/hold.png"
+              class="card-img-last"
+            />
+          </div>
+        </div>
+        <div
+          class="card card-long d-flex flex-row flex-wrap justify-content-between"
+        >
+          <p class="empty"></p>
+          <img
+            src="../assets/Leaderboard/img/selected.png"
+            alt=""
+            class="selected d-none"
+          />
+          <img src="../assets/Leaderboard/img/girl.png" class="card-img mt-1" />
+          <div>
+            <h6>Asma Akter</h6>
+            <p>Assistant</p>
+          </div>
+          <div>
+            <h6>+88 01819123456</h6>
+            <p>AAAAAAAAA</p>
+          </div>
+          <img src="../assets/Leaderboard/img/graph-arrow-up.png" />
+          <div>
+            <h6 class="text-center">122</h6>
+            <p>cccccccc</p>
+          </div>
+          <div>
+            <img
+              src="../assets/Leaderboard/img/hold.png"
+              class="card-img-last"
+            />
+          </div>
+        </div>
+        <div
+          class="card card-long d-flex flex-row flex-wrap justify-content-between"
+        >
+          <p class="empty"></p>
+          <img
+            src="../assets/Leaderboard/img/selected.png"
+            alt=""
+            class="selected d-none"
+          />
+          <img src="../assets/Leaderboard/img/girl.png" class="card-img mt-1" />
+          <div>
+            <h6>Asma Akter</h6>
+            <p>Assistant</p>
+          </div>
+          <div>
+            <h6>+88 01819123456</h6>
+            <p>AAAAAAAAA</p>
+          </div>
+          <img src="../assets/Leaderboard/img/graph-arrow-down.png" />
+          <div>
+            <h6 class="text-center">122</h6>
+            <p>cccccccc</p>
+          </div>
+          <div>
+            <img
+              src="../assets/Leaderboard/img/hold.png"
+              class="card-img-last"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="right-container container-fluid">
+      <div class="col">
+        <div class="row mb-5">
+          <div class="card card-1 p-2 mr-4">
+            <div
+              class="d-block text-center rounded-circle border border-white card-1-border"
+            >
+              <h3>365</h3>
+              <p>Deals</p>
+            </div>
+          </div>
+          <div class="card card-2 text-center">
+            <div class="card-body card-body-2">
+              <h3 class="mr-3">$1670</h3>
+              <hr class="bg-white" />
+              <p class="text-dark mr-3">Order Value</p>
+            </div>
+          </div>
+        </div>
+        <!-- end of card -->
+        <div class="row mb-5">
+          <img src="img/progressbar.png" alt="" />
+        </div>
+        <!-- image placeholder will be replaced later -->
+        <div class="mb-3 ml-3">
+          <a href="#" class="btn btn-sm bg-primary text-white">NEWS</a>
+          <a href="#" class="btn btn-sm bg-primary text-white ml-3">SHOW ALL</a>
+        </div>
+        <!-- end of links -->
+        <div class="col-lg-10 col-xl-10 col_1">
+          <a href="#" class="d-flex anchor">
+            <div class="anchor-img">
+              <img src="../assets/Leaderboard/img/girl.png" alt="" />
+            </div>
+            <div class="anchor-text mt-2">
+              <p class="name-text">Asma Akter</p>
+              <p class="small-text">Assistant</p>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-10 col-xl-10 col_1">
+          <a href="#" class=" d-flex anchor">
+            <div class="anchor-img">
+              <img src="../assets/Leaderboard/img/boyShirt.png" alt="" />
+            </div>
+            <div class="anchor-text mt-2">
+              <p class="name-text">Abdul Kalam</p>
+              <p class="small-text">Assistant</p>
+            </div>
+          </a>
+        </div>
+        <div class="col-7 col-sm-6 col-md-4 col-lg-10 col-xl-10 col_1">
+          <a href="#" class=" d-flex anchor">
+            <div class="anchor-img">
+              <img src="../assets/Leaderboard/img/boyTshirt.png" alt="" />
+            </div>
+            <div class="anchor-text mt-2">
+              <p class="name-text">Mostafiz Rahman</p>
+              <p class="small-text">Assistant</p>
+            </div>
+          </a>
+        </div>
+        <div class="col-6 col-sm-6 col-md-4 col-lg-10 col-xl-10 col_1">
+          <a href="#" class=" d-flex anchor">
+            <div class="anchor-img">
+              <img src="../assets/Leaderboard/img/girl.png" alt="" />
+            </div>
+            <div class="anchor-text mt-2">
+              <p class="name-text">Asma Akter</p>
+              <p class="small-text">Assistant</p>
+            </div>
+          </a>
+        </div>
+        <div class="col-6 col-sm-6 col-md-4 col-lg-10 col-xl-10 col_1">
+          <a href="#" class=" d-flex anchor">
+            <div class="anchor-img">
+              <img src="../assets/Leaderboard/img/girl.png" alt="" />
+            </div>
+            <div class="anchor-text mt-2">
+              <p class="name-text">Asma Akter</p>
+              <p class="small-text">Assistant</p>
+            </div>
+          </a>
+        </div>
+        <div class="col-6 col-sm-6 col-md-4 col-lg-10 col-xl-10 col_1">
+          <a href="#" class=" d-flex anchor">
+            <div class="anchor-img">
+              <img src="../assets/Leaderboard/img/girl.png" alt="" />
+            </div>
+            <div class="anchor-text mt-2">
+              <p class="name-text">Asma Akter</p>
+              <p class="small-text">Assistant</p>
+            </div>
+          </a>
+        </div>
+        <div class="col-6 col-sm-6 col-md-4 col-lg-10 col-xl-10 col_1">
+          <a href="#" class=" d-flex anchor">
+            <div class="anchor-img">
+              <img src="../assets/Leaderboard/img/girl.png" alt="" />
+            </div>
+            <div class="anchor-text mt-2">
+              <p class="name-text">Asma Akter</p>
+              <p class="small-text">Assistant</p>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeaderBoardContacts'
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  background-color: #f8f8f8;
+  border-radius: 10%;
+  border: none;
+  margin: 20px;
+}
+
+.card h4 {
+  color: #00aeef;
+}
+
+.card-img-top {
+  width: 100px;
+}
+.graph {
+  width: 100px;
+}
+
+.card-upper {
+  border: 1px solid black;
+  border-left: 0px;
+  border-right: 0px;
+}
+.card-upper-text {
+  margin-top: 10px;
+  width: 120px;
+}
+
+.between {
+  height: 100%;
+  border: 2px solid white;
+}
+
+h4 {
+  color: #808080;
+  font-weight: bold;
+}
+p {
+  color: #808080;
+  font-weight: bold;
+}
+.upper-number {
+  font-size: 20px;
+}
+
+.card-long {
+  border-radius: 30px;
+  margin: 10px;
+}
+
+.card-img {
+  width: 100px;
+  height: 70px;
+}
+.card-img-last {
+  height: 40px;
+}
+
+.card div {
+  margin: auto;
+}
+h5,
+h6 {
+  color: #808080;
+  font-weight: bold;
+}
+
+.selected {
+  margin: auto 0;
+  height: 25px;
+}
+.empty {
+  width: 30px;
+}
+.active .empty {
+  display: none;
+}
+
+@media only screen and (min-width: 960px) {
+  .card-long {
+    height: 80px;
+  }
+}
+
+@media only screen and (max-width: 466px) {
+  .card-img {
+    margin: 5px auto;
+  }
+}
+
+.left-container {
+  width: 75%;
+}
+
+.right-container {
+  border-left: 5px solid #999;
+  width: 25%;
+}
+
+@media only screen and (max-width: 992px) {
+  .right-container {
+    display: none;
+  }
+  .left-container {
+    width: 100%;
+  }
+  .cards {
+    align-items: center;
+  }
+}
+
+$primary-font-color: #fff;
+$secondary-font-color: #808080;
+$card-1-color: #ed1e79;
+$card-2-color: #00aeef;
+
+.card-1,
+.card-2 {
+  width: 8rem;
+  height: 6rem;
+  border-radius: 1rem;
+  color: $primary-font-color;
+}
+.card-1 {
+  background-color: $card-1-color;
+}
+.card-2 {
+  background-color: $card-2-color;
+}
+.card-text {
+  font-size: 0.7rem;
+}
+.card-body h3,
+p {
+  margin-top: -8px;
+}
+hr {
+  width: 8rem;
+  margin-left: -21px;
+}
+.small-text {
+  font-size: 0.8rem;
+  color: $secondary-font-color;
+  margin-top: -15px;
+}
+.name-text {
+  font-weight: bold;
+}
+.anchor img {
+  width: 4rem;
+}
+.card-1-border {
+  width: 5rem;
+}
+.col_1 {
+  margin-top: 6px;
+  a {
+    text-decoration: none;
+    &:hover {
+      box-shadow: 5px 5px #e5e5e5;
+      transition-duration: 0.5s;
+    }
+  }
+
+  @media (max-width: 480px) {
+    .name-text {
+      margin-top: 7px;
+      font-size: 0.7rem;
+    }
+  }
+}
+</style>
